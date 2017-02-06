d3bc947cf889a7ad368b375fca641e2864b2989d
amankumarbhatia155 <amanbhatia155@gmail.com>
2017-02-06 15:16:32 +0530

    Latest

--- Test/src/Test.java
+++ Test/src/Test.java
@@ -5,7 +5,6 @@ public class Test {
 		// TODO Auto-generated method stub
 System.out.println("Hello");
 System.out.println("Anil Yadav");
-System.out.println("Fuck Off");
 System.out.println("😛");
 	}
 
